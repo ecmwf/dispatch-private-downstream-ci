d3a3c6c6f8f4dd2efd759f5fc420b5508f60d708
Dusan Figala <dusanfigala@protonmail.com>
2023-02-17 16:10:13 +0100

    Dispatch workflow

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import argparse
 import json
+import sys
+import uuid
+
+import requests
+
+# Constants
+GITHUB_BASE_URL = "https://api.github.com"
 
 
 def start_group(name: str) -> None:
@@ -31,13 +38,10 @@ def debug(msg: str) -> None:
     print(f"::debug::{msg}")
 
 
-def dispatch_workflow(owner: str, repo: str, event_type: str, payload: dict):
-    pass
-
-
 @group("Inputs")
-def get_args():
+def get_args() -> dict:
     parser = argparse.ArgumentParser()
+    parser.add_argument("token", type=str)
     parser.add_argument("owner", type=str)
     parser.add_argument("repo", type=str)
     parser.add_argument("event_type", type=str)
@@ -48,13 +52,47 @@ def get_args():
     for k, v in vars(args).items():
         print(f"{k}: {v}")
 
-    return args
+    return vars(args)
+
+
+@group("Dispatch workflow")
+def dispatch_workflow(
+    session: requests.Session,
+    owner: str,
+    repo: str,
+    event_type: str,
+    payload: dict,
+    **kwargs,
+):
+    url = f"{GITHUB_BASE_URL}/repos/{owner}/{repo}/dispatches"
+    id = str(uuid.uuid4())
+    data = {
+        "event_type": event_type,
+        "client_payload": {
+            "id": id,
+            "inputs": payload,
+        },
+    }
+    print(f"==> POST: {url}\n", data)
+    response = session.post(url, data=json.dumps(data))
+
+    if response.status_code == requests.codes.no_content:
+        print(f"Workflow triggered: {id}")
+        return id
+    else:
+        print(f"==> {response.status_code}: Error dispatching workflow!")
+        print(response.json())
+        sys.exit(1)
 
 
 def main():
-    args = get_args()
+    inputs = get_args()
 
-    # dispatch wf with random id
+    # Setup Github session auth
+    session = requests.Session()
+    session.headers = {"Authorization": f"token {inputs.get('token')}"}
+
+    dispatch_workflow(session, **inputs)
     # poll api for wf with id (timeout)
     # poll api for wf status (timeout)
     # exit ok/ko, link to wf
